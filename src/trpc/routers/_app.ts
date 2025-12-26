f72c12c8e5fc05a83263a9cf99a7afb4a3858c6c
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2025-12-26 16:27:02 +0530

    06:background-jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,24 +1,28 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter, protectedProcedures } from '../init';
 import prisma from '@/lib/db';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedures
+  getWorkflows: protectedProcedures
     
     .query(async({ctx}) => {
       if(!ctx){
         return []
       }
       
-      const res =await  prisma.user.findMany({
-        where:{
-          id:ctx?.auth?.user.id 
-        }
-      }
-      );
+      const res =await  prisma.workflow.findMany()
       
       return res;
       
     }),
+  createWorkflow:protectedProcedures.mutation(async()=>{
+   await inngest.send({
+    name:"test/hello.world",
+    data:{
+      email:"rocky@gmail.com"
+    }
+   })
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
